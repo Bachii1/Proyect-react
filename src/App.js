636afbc72baabcd1d033959c7bd751831e2f1cf4
unknown <marianosoler2000@hotmail.com>
2021-09-23 19:20:41 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/form">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+function Form() {
+  return <h1>Form</h1>;
+}
+
+function Users() {
+  return <h1>Users</h1>;
+}
